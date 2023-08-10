4099248ac3933312e783aefbe9bb1b5bd55acf31
apoko <apokora@itsap.pl>
2023-08-10 20:23:56 +0200

    WILD-66 Created main page and logout page

---
+++ wild-event-back-office-application/src/components/header/HeaderComponent.jsx
@@ -0,0 +1,24 @@
+import { Box, AppBar, CssBaseline, Toolbar, Typography, IconButton} from "@mui/material";
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import { useDarkMode } from "../../components/darkMode/DarkModeProvider";
+
+export const HeaderComponent = () => {
+	const { darkMode, toggleDarkMode } = useDarkMode();
+	console.log("dark mode: ", darkMode);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+				<CssBaseline />
+				<AppBar position='fixed'sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+					<Toolbar>
+						<Typography variant='h6' noWrap component='div'>
+							Elephant APP
+						</Typography>
+						<IconButton color="inherit" onClick={toggleDarkMode} sx={{marginLeft: "auto"}}>
+            <Brightness4Icon />
+          </IconButton>
+					</Toolbar>
+				</AppBar>
+        </Box>
+  )
+}
--- wild-event-back-office-application/src/router.js
+++ wild-event-back-office-application/src/router.js
@@ -1,73 +1,90 @@
-import { createBrowserRouter } from "react-router-dom";
-import Test from "./test";
-import ErrorPage from "./pages/ErrorPage";
-import Layout from "./pages/layout/Layout";
-import LoginForm from "./components/loginForm/LoginForm";
-
+import { createBrowserRouter } from "react-router-dom"
+import Test from "./test"
+import EventList from "./pages/EventList"
+import ErrorPage from "./pages/ErrorPage"
+import Layout from "./pages/layout/Layout"
+import LoginForm from "./components/loginForm/LoginForm"
+import { LogoutPage } from "./pages/LogoutPage"
+import { MainPage } from "./pages/MainPage"
+import { DarkModeProvider } from "./components/darkMode/DarkModeProvider"
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/signup",
-        element: <LoginForm />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/my-events/event",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/my-events/event/:id",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/event-management/event",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/event-management/event/id",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/event-management/event/waiting",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/staff-management/staff",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/staff-management/staff/id",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/map-config/location",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/map-config/location/id",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/map-config/map",
-        element: <Test />,
-        errorElement: <ErrorPage />,
-      }
-    ],
-  },
+	{
+		path: "/",
+		element: (
+			<DarkModeProvider>
+				<Layout />
+			</DarkModeProvider>
+		),
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/signup",
+				element: <LoginForm />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/main",
+				element: <MainPage />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/my-events/event",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/my-events/event/:id",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/event-management/event",
+				element: <EventList />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/event-management/event/id",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/event-management/event/waiting",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/staff-management/staff",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/staff-management/staff/id",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/map-config/location",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/map-config/location/id",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/map-config/map",
+				element: <Test />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/logout",
+				element: <LogoutPage />, // Dodaj tę ścieżkę
+				errorElement: <ErrorPage />,
+			},
+		],
+	},
 ])
 
-export default router;
+export default router
