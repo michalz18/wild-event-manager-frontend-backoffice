cd27644278b10b87f1395e95313e154b7b783302
Michał Zuzak <michalzuzak.2@gmail.com>
2023-08-28 21:41:13 +0200

    WILD 65 Fetch for adding user

--- wild-event-back-office-application/src/services/EmployeeManagement.js
+++ wild-event-back-office-application/src/services/EmployeeManagement.js
@@ -9,3 +9,23 @@ const getAllActiveUsers = async () => {
         console.error("Cannot fetch active users!");
     }
 }
+
+const addUser = async (userDTO) => {
+    try {
+        const response = await fetch(`${process.env.REACT_APP_ADD_USER}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userDTO)
+        });
+        if (!response.ok) {
+            throw new Error("Failed to add user!");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Cannot add user:" , error);
+    }
+
+    
+}
