9870d4f48a3eeb65c72b02b8c0cdf2eb7846d54d
Michał Zuzak <michalzuzak.2@gmail.com>
2023-08-28 21:49:40 +0200

    WILD 65 Fetch for updating user

--- wild-event-back-office-application/src/services/EmployeeManagement.js
+++ wild-event-back-office-application/src/services/EmployeeManagement.js
@@ -27,5 +27,22 @@ const addUser = async (userDTO) => {
         console.error("Cannot add user:" , error);
     }
 
-    
+const updateUser = async (userId, userDTO) => {
+    try {
+        const response = await fetch(`${process.env.REACT_APP_UPDATE_USER}${userId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userDTO)
+        });
+        if (!response.ok) {
+            throw new Error("There is an issue with updating user!");
+        }
+    } catch (error) {
+        console.error("Cannot update user:", error);
+    }
+}
+
+
 }
