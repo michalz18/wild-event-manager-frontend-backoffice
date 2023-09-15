c90367365a0f934ccec2f7fca6294ad181257351
Michał Zuzak <michalzuzak.2@gmail.com>
2023-09-15 11:33:49 +0200

    WILD-73 Formik added to email and password validation in loginForm and handling situation when user types in incorrect password

--- wild-event-back-office-application/src/components/loginForm/LoginForm.jsx
+++ wild-event-back-office-application/src/components/loginForm/LoginForm.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { useUser } from '../../services/useUser';
 import { loginUser } from '../../services/LoginService';
 import {
@@ -22,25 +24,33 @@ import backgroundImage from '../../assets/logo.png';
 
 const defaultTheme = createTheme();
 
+const validationSchema = Yup.object({
+  email: Yup.string().email('Invalid email format').required('Required'),
+  password: Yup.string().required('Required'),
+});
+
 const LoginForm = () => {
   const navigate = useNavigate();
   const { login } = useUser();
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const email = data.get('email');
-    const password = data.get('password');
-
-    try {
-      const response = await loginUser(email, password);
-      localStorage.setItem('token', response.token);
-      login(response);
-      navigate('/main');
-    } catch (error) {
-      console.error('Error while logging in:', error);
-    }
-  };
+  const [loginError, setLoginError] = useState(null);
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        const response = await loginUser(values.email, values.password);
+        localStorage.setItem('token', response.token);
+        login(response);
+        navigate('/main');
+      } catch (error) {
+        console.error('Error while logging in:', error);
+        setLoginError('Invalid email or password');
+      }
+    },
+  });
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -55,9 +65,38 @@ const LoginForm = () => {
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
-              <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+            <Box component="form" noValidate onSubmit={formik.handleSubmit} sx={{ mt: 1 }}>
+            {loginError && <Typography color="error">{loginError}</Typography>}
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+              />
               <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
               <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
                 Sign In
