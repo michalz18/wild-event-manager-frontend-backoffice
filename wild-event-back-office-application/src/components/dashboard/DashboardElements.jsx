1c7e587b57a8b90e78510677a2e4517613bb34f2
Michał Zuzak <michalzuzak.2@gmail.com>
2023-09-19 23:09:31 +0200

    WILD-85 requiredRole key added to all items of menu elements

--- wild-event-back-office-application/src/components/dashboard/DashboardElements.jsx
+++ wild-event-back-office-application/src/components/dashboard/DashboardElements.jsx
@@ -4,10 +4,10 @@ import PersonIcon from "@mui/icons-material/Person";
 import MapIcon from "@mui/icons-material/Map";
 
 const itemList = [
-  { text: "Event management", icon: <EventIcon />, path: "/event-management/event" },
-  { text: "My events", icon: <NotificationIcon />, path: "/my-events/event" },
-  { text: "Employee management", icon: <PersonIcon />, path: "/staff-management" },
-  { text: "Map configuration", icon: <MapIcon />, path: "/map-config/map" },
+  { text: "Event management", icon: <EventIcon />, path: "/event-management/event", requiredRole: 'EVENT-MANAGEMENT' },
+  { text: "My events", icon: <NotificationIcon />, path: "/my-events/event", requiredRole: 'MY-EVENTS' },
+  { text: "Employee management", icon: <PersonIcon />, path: "/staff-management", requiredRole: 'EMPLOYEE-MANAGEMENT' },
+  { text: "Map configuration", icon: <MapIcon />, path: "/map-config/map", requiredRole: 'MAP-CONFIGURATION' },
 ];
 
 export default itemList; 
