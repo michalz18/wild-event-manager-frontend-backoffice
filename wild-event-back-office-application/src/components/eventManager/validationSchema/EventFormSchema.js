51c4ee6223859f13dee460e39a5c04c1d0422a65
marcinlewinski <marcinlewinski71@gmail.com>
2023-09-21 10:14:40 +0200

    WILD-83 Formik validation for EventForm component has been implemented

--- wild-event-back-office-application/src/components/eventManager/validationSchema/EventFormSchema.js
+++ wild-event-back-office-application/src/components/eventManager/validationSchema/EventFormSchema.js
@@ -1,30 +1,38 @@
 import * as yup from "yup";
-
-const yupTextField = yup
-    .string()
-    .min(5, "Too Short!")
-    .max(100, "Too Long!")
-    .required("This field is required!");
+import dayjs from 'dayjs';
 
 const customDateFormat = "YYYY-MM-DDTHH:mm:ss";
 
-const customDateValidator = (value) => {
-    if (!yup.string().matches(new RegExp(`^${customDateFormat}$`)).isValidSync(value)) {
-        return `Date must be in the format ${customDateFormat}`;
-    }
-}
 
 const basicSchema = yup.object().shape({
-    title: yupTextField,
-    description: yupTextField,
-    dateRange: yup.object().shape({
-        startsAt: yup.string().test("date-format", "Invalid date format", customDateValidator),
-        endsAt: yup.string().test("date-format", "Invalid date format", customDateValidator),
-    }),
+    title: yup.string().min(5, "Too Short!").max(100, "Too Long!").required("This field is required!"),
+    description: yup.string().min(5, "Too Short!").max(100, "Too Long!").required("This field is required!"),
+    // dateRange: yup.object().shape({
+    //     startsAt: yup.date()
+    //         .test('date-future', 'Start date cannot be in the past', (value) => {
+                
+    //             const currentDate = dayjs();
+    //             const startDate = dayjs(value);
+    //             return startDate >= currentDate;
+    //         }),
+    //     endsAt: yup.date()
+    //         .test('date-future', 'End date cannot be in the past', (value) => {
+    //             const currentDate = dayjs();
+    //             const endDate = dayjs(value);
+    //             return endDate >= currentDate;
+    //         })
+    //         .test('date-range', 'End date must be after start date', function (value) {
+    //             const { startsAt } = this.parent;
+    //             if (startsAt && value) {
+    //                 return dayjs(value).isAfter(dayjs(startsAt));
+    //             }
+    //             return true;
+    //         }),
+    // }),
+    
     locationId: yup.string().required("This field is required!"),
     organizers: yup.array().of(yup.string()).min(1, "At least one organizer is required!"),
     openToPublic: yup.boolean(),
 });
-
 
 export default basicSchema;
